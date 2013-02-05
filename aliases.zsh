e1e8440dd7c3243d00916be2ff4871f3b8714ea4
Brian Hartvigsen <brian.andrew@brianandjenny.com>
2013-02-04 22:19:22 -0700

    Make these fail gracefully

--- aliases.zsh
+++ aliases.zsh
@@ -1,6 +1,15 @@
 # RunSnakeRun on OS X barfs unless I use runsnake32
 alias runsnake=runsnake32
-alias grep='ag'
-alias ack='ag'
-alias ack_='command ack'
-alias grep_='command grep'
+if [[ $(whence ag) != "" ]]; then
+    alias grep='ag'
+    alias ack='ag'
+    alias ack_='command ack'
+    alias grep_='command grep'
+elif [[ $(whence ack) != "" ]]; then
+    alias grep='ack'
+    alias ag='ack'
+    alias grep_='command grep'
+else
+    alias ag='grep'
+    alias ack='grep'
+fi

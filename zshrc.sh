4b1116d7c73eece668ab8179eede6df3f2bab2e3
Brian Hartvigsen <brian.andrew@brianandjenny.com>
2014-01-05 00:11:28 -0700

    Pulling in some upstream changes and adding more plugins

--- zshrc.sh
+++ zshrc.sh
@@ -1,3 +1,4 @@
+# This is symlinked to ~/.zshrc
 # Path to your oh-my-zsh configuration.
 ZSH=$HOME/.oh-my-zsh
 
@@ -15,8 +16,11 @@ alias ohmyzsh="st ~/.oh-my-zsh"
 # Set to this to use case-sensitive completion
 # CASE_SENSITIVE="true"
 
-# Comment this out to disable weekly auto-update checks
+# Uncomment this to disable bi-weekly auto-update checks
 # DISABLE_AUTO_UPDATE="true"
+
+# Uncomment to change how often before auto-updates occur? (in days)
+# export UPDATE_ZSH_DAYS=13
 
 # Uncomment following line if you want to disable colors in ls
 # DISABLE_LS_COLORS="true"
@@ -24,8 +28,21 @@ alias ohmyzsh="st ~/.oh-my-zsh"
 # Uncomment following line if you want to disable autosetting terminal title.
 # DISABLE_AUTO_TITLE="true"
 
+# Uncomment following line if you want to disable command autocorrection
+# DISABLE_CORRECTION="true"
+
 # Uncomment following line if you want red dots to be displayed while waiting for completion
-# COMPLETION_WAITING_DOTS="true"
+COMPLETION_WAITING_DOTS="true"
+
+# Uncomment following line if you want to disable marking untracked files under
+# VCS as dirty. This makes repository status check for large repositories much,
+# much faster.
+# DISABLE_UNTRACKED_FILES_DIRTY="true"
+
+# Uncomment following line if you want to  shown in the command execution time stamp
+# in the history command output. The optional three formats: "mm/dd/yyyy"|"dd.mm.yyyy"|
+# yyyy-mm-dd
+# HIST_STAMPS="mm/dd/yyyy"
 
 SVN_SHOW_BRANCH="true"
 MACPORTS_MAINTAINED=(libcurvecpr libsodium libchloride ldns ldns-tools drill gcalcli py-google-api py-icalendar py-oauth2client py-parsedatetime py-uritemplate py-vobject the_silver_searcher)
@@ -33,8 +50,25 @@ MACPORTS_MAINTAINED=(libcurvecpr libsodium libchloride ldns ldns-tools drill gca
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(battery git github jira macports nyan osx python screen sublime svn urltools)
+plugins=(battery common-aliases encode64 extract gem git github history history-substring-search jira node npm macports nyan osx python redis-cli rsync ruby screen sublime sudo svn systemadmin theme urltools xcode)
 
 source $ZSH/oh-my-zsh.sh
 
-# Customize to your needs...
+# User configuration
+
+#doing this in zshenv
+#export PATH=$HOME/bin:/usr/local/bin:$PATH
+# export MANPATH="/usr/local/man:$MANPATH"
+
+# # Preferred editor for local and remote sessions
+# if [[ -n $SSH_CONNECTION ]]; then
+#   export EDITOR='vim'
+# else
+#   export EDITOR='mvim'
+# fi
+
+# Compilation flags
+# export ARCHFLAGS="-arch x86_64"
+
+# ssh
+# export SSH_KEY_PATH="~/.ssh/dsa_id"

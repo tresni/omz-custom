022e865f12e8887e8f5eb0400317d6a9fcfbe1f1
Brian Hartvigsen <brian.andrew@brianandjenny.com>
2014-06-06 20:14:31 -0700

    Not sure why this python declaration is here so let's remove it

--- zshenv.sh
+++ zshenv.sh
@@ -1,7 +1,2 @@
 # This is symlinked to ~/.zshenv
 export PATH=/Users/$USER/.bin:/opt/local/bin:/opt/local/sbin:/usr/local/MacGPG2/bin:$PATH
-
-# Setting PATH for Python 2.7
-# The orginal version is saved in .zprofile.pysave
-PATH="/Library/Frameworks/Python.framework/Versions/2.7/bin:${PATH}"
-export PATH

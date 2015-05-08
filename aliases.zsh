408f340fc5bb31f1eba6623c168cff6009f2a934
Brian Hartvigsen <brian.andrew@brianandjenny.com>
2015-05-08 13:39:37 -0700

    Remove this MacPorts alias

--- aliases.zsh
+++ aliases.zsh
@@ -1,10 +1,5 @@
 # RunSnakeRun on OS X barfs unless I use runsnake32
 alias runsnake=runsnake32
-
-if [[ $(whence pip-2.7) != "" ]]; then
-    alias pip='sudo pip-2.7'
-fi
-
 alias zshconfig="\$EDITOR ~/.zshrc"
 alias zshrc="\$EDITOR ~/.zshrc"
 alias ohmyzsh="\$EDITOR ~/.oh-my-zsh"
